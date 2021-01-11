df6d601780c962a13174f9d25126d6c2fa4b63b6
bbworld <vwangsf@gmail.com>
2021-01-10 16:15:40 -0800

    Update submodules

--- scripts/run_sim.sh
+++ scripts/run_sim.sh
@@ -9,10 +9,6 @@ tmux new-session -d
 tmux new-session -d
 tmux selectp -t 0    # select the first (0) pane
 tmux splitw -h -p 50 # split it into two halves
-tmux selectp -t 0    # select the first (0) pane
-tmux splitw -v -p 50 # split it into two halves
-tmux selectp -t 2    # select the new, second (2) pane
-tmux splitw -v -p 50 # split it into two halves
 
 # Source setup scripts
 tmux selectp -t 0
@@ -35,18 +31,18 @@ tmux send-keys "source install/setup.sh" C-m
 tmux send-keys "ros2 run ros1_bridge dynamic_bridge" C-m
 
 # Run sub control node
-tmux selectp -t 2
-tmux send-keys "cd ros2_ws" C-m
-tmux send-keys "source install/setup.sh" C-m
-tmux send-keys "ros2 run sub_control service --ros-args -p SIM:=true" C-m
-
-# Prepare 4th pane to run ros2 stuff
-tmux selectp -t 3
-tmux send-keys "cd ros2_ws" C-m
-tmux send-keys "source install/setup.sh" C-m
-echo "Waiting 15 seconds for sim to start..."
-sleep 15
-tmux send-keys 'ros2 service call /control_write sub_control_interfaces/srv/ControlWrite "{data: p 0.15\n}"' C-m
+# tmux selectp -t 2
+# tmux send-keys "cd ros2_ws" C-m
+# tmux send-keys "source install/setup.sh" C-m
+# tmux send-keys "ros2 run sub_control service --ros-args -p SIM:=true" C-m
+#
+# # Prepare 4th pane to run ros2 stuff
+# tmux selectp -t 3
+# tmux send-keys "cd ros2_ws" C-m
+# tmux send-keys "source install/setup.sh" C-m
+# echo "Waiting 15 seconds for sim to start..."
+# sleep 15
+# tmux send-keys 'ros2 service call /control_write sub_control_interfaces/srv/ControlWrite "{data: p 0.15\n}"' C-m
 
 # From here, your ROS2 stuff should work!
 tmux a
